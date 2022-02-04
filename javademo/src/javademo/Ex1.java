b087e5a60f4829a7b102a06679db51bf54e121c8
vaishu vollala <vaishu vollala@LAPTOP-U09TFG76>
2022-02-04 11:44:03 +0530

    my commit

---
+++ javademo/src/javademo/Ex1.java
@@ -0,0 +1,17 @@
+package javademo;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int customerId=2345;
+		String customerName="varun";
+	    String customerloc="Hyderabad";
+	    int customersal=30000;
+	    System.out.println(customerId);
+	    System.out.println(customerName);
+	    System.out.println(customerloc);
+	    System.out.println(customersal);
+
+	}
+
+}
